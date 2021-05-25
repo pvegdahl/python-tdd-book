2018e1affa4c38519c9dd03c9f6278644874fb97
Philip Vegdahl <pvegdahl@gmail.com>
2021-05-25 16:54:26 -0400

    Update test to use unittest.  It still fails.

--- functional_tests.py
+++ functional_tests.py
@@ -1,29 +1,41 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
 
-# Go to the webpage to check out the app
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-# The page title mentions to-do lists
-assert 'To-Do' in browser.title
+    def tearDown(self) -> None:
+        # Satisfied, she goes back to sleep
+        self.browser.quit()
 
-# The user is invited to enter a to-do immediately
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Go to the webpage to check out the app
+        self.browser.get("http://localhost:8000")
 
-# She enters "Buy peacock feathers" into a text box
+        # The page title mentions to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
 
-# She hits enter, the page updates, and now lists:
-# "1: Buy peacock feathers" as a to-do list item
+        # The user is invited to enter a to-do immediately
 
-# There is still a text box to enter another item.  She
-# enters "Use peacock feathers to make a fly"
+        # She enters "Buy peacock feathers" into a text box
 
-# The page updates again, and now shows both items
+        # She hits enter, the page updates, and now lists:
+        # "1: Buy peacock feathers" as a to-do list item
 
-# There is now a unique URL to use to save her to-do list
-# There is explanatory text to that effect.
+        # There is still a text box to enter another item.  She
+        # enters "Use peacock feathers to make a fly"
 
-# The user visits that unique URL and the todo list is still there!
+        # The page updates again, and now shows both items
 
-# Satisfied, she goes back to sleep
-browser.quit()
+        # There is now a unique URL to use to save her to-do list
+        # There is explanatory text to that effect.
+
+        # The user visits that unique URL and the todo list is still there!
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
