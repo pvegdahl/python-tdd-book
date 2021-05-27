dc7e26c6b11e74ee7034cbc800fbd5198c6d034a
Philip Vegdahl <pvegdahl@gmail.com>
2021-05-26 21:58:37 -0400

    Added to the template and improved a test message

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from selenium import webdriver
@@ -35,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows), "New to-do did not appear in table")
 
         # There is still a text box to enter another item.  She
         # enters "Use peacock feathers to make a fly"
