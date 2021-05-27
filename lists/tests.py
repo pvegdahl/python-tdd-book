a6353893c4b84634664e744d517c19a0c6936bc1
Philip Vegdahl <pvegdahl@gmail.com>
2021-05-27 09:34:01 -0400

    Closer to working, but still a little broken

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     def test_homepage_returns_correct_html(self):
         self.assertTemplateUsed(self.client.get("/"), "home.html")
+
+    def test_can_save_a_post_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, "home.html")
