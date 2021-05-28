a68029e849341bb953ffebb6ccc7463e66b0b9c3
Philip Vegdahl <pvegdahl@gmail.com>
2021-05-27 21:27:06 -0400

    Save progress before migrations.

--- functional_tests.py
+++ functional_tests.py
@@ -27,25 +27,17 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual("Enter a to-do item", input_box.get_attribute("placeholder"))
 
         # She enters "Buy peacock feathers" into a text box
-        input_box.send_keys("Buy peacock feathers")
-
         # She hits enter, the page updates, and now lists:
         # "1: Buy peacock feathers" as a to-do list item
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self._send_input_and_sleep("Buy peacock feathers")
 
         self._check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box to enter another item.  She
         # enters "Use peacock feathers to make a fly"
-        input_box = self.browser.find_element_by_id("id_new_item")
-        input_box.send_keys("Use peacock feathers to make a fly")
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self._send_input_and_sleep("Use peacock feathers to make a fly")
 
         # The page updates again, and now shows both items
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
         self._check_for_row_in_list_table("1: Buy peacock feathers")
         self._check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
@@ -55,10 +47,16 @@ class NewVisitorTest(unittest.TestCase):
 
         # The user visits that unique URL and the todo list is still there!
 
-    def _check_for_row_in_list_table(self, row_text):
+    def _check_for_row_in_list_table(self, row_text: str) -> None:
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn(row_text, [row.text for row in rows])
+
+    def _send_input_and_sleep(self, input_text: str) -> None:
+        input_box = self.browser.find_element_by_id("id_new_item")
+        input_box.send_keys(input_text)
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -9,3 +10,22 @@ class HomePageTest(TestCase):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_text = "The first (ever) list item"
+        first_item = Item()
+        first_item.text = first_text
+        first_item.save()
+
+        second_text = "Item the second"
+        second_item = Item()
+        second_item.text = second_text
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(2, len(saved_items))
+
+        self.assertEqual(first_text, saved_items[0].text)
+        self.assertEqual(second_text, saved_items[1].text)
