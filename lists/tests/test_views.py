50420c437ac4e22f3588f7fe4b23eb7d5e65ddbe
Philip Vegdahl <pvegdahl@gmail.com>
2021-06-16 09:47:05 -0400

    Split list tests into multiple files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -39,35 +39,6 @@ class ListViewTest(TestCase):
         self.assertEqual(correct_list, response.context["list"])
 
 
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_text = "The first (ever) list item"
-        first_item = Item()
-        first_item.text = first_text
-        first_item.list = list_
-        first_item.save()
-
-        second_text = "Item the second"
-        second_item = Item()
-        second_item.text = second_text
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(list_, saved_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(2, len(saved_items))
-
-        self.assertEqual(first_text, saved_items[0].text)
-        self.assertEqual(list_, saved_items[0].list)
-        self.assertEqual(second_text, saved_items[1].text)
-        self.assertEqual(list_, saved_items[1].list)
-
-
 class NewListTest(TestCase):
     def test_can_save_a_post_request(self):
         item_text = "A new list item"
