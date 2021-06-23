00da933e1174da7887df0ece92322aa75f6e3c60
Philip Vegdahl <pvegdahl@gmail.com>
2021-06-23 10:13:06 -0400

    Updated some model code

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -41,3 +41,34 @@ class ListAndItemModelTest(TestCase):
     def test_get_absolute_url(self):
         the_list = List.objects.create()
         self.assertEqual(f"/lists/{the_list.id}/", the_list.get_absolute_url())
+
+    def test_duplicate_items_are_invalid(self):
+        the_list = List.objects.create()
+        text = "Duplicate text"
+        Item.objects.create(text=text, list=the_list)
+        with self.assertRaises(ValidationError):
+            item = Item(text=text, list=the_list)
+            item.full_clean()
+
+    @staticmethod
+    def test_CAN_save_duplicate_items_to_different_lists():
+        list_1 = List.objects.create()
+        text = "Duplicate text"
+        Item.objects.create(text=text, list=list_1)
+
+        list_2 = List.objects.create()
+        item = Item(text=text, list=list_2)
+        item.full_clean()  # Should not raise an exception
+
+    def test_list_ordering(self):
+        the_list = List.objects.create()
+        item_1 = Item.objects.create(list=the_list, text="one")
+        item_2 = Item.objects.create(list=the_list, text="two")
+        item_3 = Item.objects.create(list=the_list, text="three")
+        self.assertEqual([item_1, item_2, item_3], list(Item.objects.all()))
+
+    def test_item_string_representation(self):
+        text = "some text"
+        item = Item(text=text)
+        self.assertEqual(text, str(item))
+
