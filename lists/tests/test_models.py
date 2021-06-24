c2eb7a4c0e1af0729c2115ec0d4419f59140f392
Philip Vegdahl <pvegdahl@gmail.com>
2021-06-24 09:18:55 -0400

    Reworked model tests

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -4,32 +4,16 @@ from lists.models import Item, List
 
 
 class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        the_list = List()
-        the_list.save()
+    def test_default_text(self):
+        self.assertEqual("", Item().text)
 
-        first_text = "The first (ever) list item"
-        first_item = Item()
-        first_item.text = first_text
-        first_item.list = the_list
-        first_item.save()
-
-        second_text = "Item the second"
-        second_item = Item()
-        second_item.text = second_text
-        second_item.list = the_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(the_list, saved_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(2, len(saved_items))
-
-        self.assertEqual(first_text, saved_items[0].text)
-        self.assertEqual(the_list, saved_items[0].list)
-        self.assertEqual(second_text, saved_items[1].text)
-        self.assertEqual(the_list, saved_items[1].list)
+    def test_item_is_related_to_list(self):
+        the_list = List.objects.create()
+        item_1 = Item(text="whatever", list=the_list)
+        item_1.save()
+        item_2 = Item(text="whatever else", list=the_list)
+        item_2.save()
+        self.assertEqual([item_1, item_2], list(the_list.item_set.all()))
 
     def test_cannot_save_empty_items(self):
         the_list = List.objects.create()
