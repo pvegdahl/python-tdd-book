c5d79dbaff2fab464e54ccefd9de06cbe6ca82f5
Philip Vegdahl <pvegdahl@gmail.com>
2021-06-28 09:45:14 -0400

    Strip out console.log statements

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,9 +1,5 @@
 var initialize = function () {
-    console.log("Initialize called")
     $('input[name="text"]').on('keypress', function () {
-        console.log("In keypress handler")
         $('.has-error').hide();
     });
-
 }
-console.log("list.js loaded")
