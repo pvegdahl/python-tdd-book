fa5ec79b462545465084226c60d92a673620900d
Philip Vegdahl <pvegdahl@gmail.com>
2021-06-29 09:57:39 -0400

    Add Token model with first passing test

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -7,3 +9,10 @@ class User(models.Model):
     USERNAME_FIELD = "email"
     is_anonymous = False
     is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.UUIDField(default=uuid.uuid4)
+
+
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,14 +1,24 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from accounts.models import Token
+
 User = get_user_model()
+
+EMAIL = "a@b.com"
 
 
 class UserModelTest(TestCase):
     def test_user_is_valid_with_email_only(self):
-        user = User(email="a@b.com")
+        user = User(email=EMAIL)
         user.full_clean()  # Should not raise an exception
 
     def test_email_is_primary_key(self):
-        email = "a@b.com"
-        self.assertEqual(User(email=email).pk, email)
+        self.assertEqual(User(email=EMAIL).pk, EMAIL)
+
+
+class TokenModelTest(TestCase):
+    def test_links_user_with_unique_auto_generated_uid(self):
+        token_1 = Token.objects.create(email=EMAIL)
+        token_2 = Token.objects.create(email=EMAIL)
+        self.assertNotEqual(token_1.uid, token_2.uid)
