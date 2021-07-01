b0c161c92d488f1430a585f7d6d2e79119fce3d7
Philip Vegdahl <pvegdahl@gmail.com>
2021-07-01 09:40:51 -0400

    Fixed that particular bug (typo)

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 
 
-EMAIL_SUBJECT = "Your login link for Superlists",
+EMAIL_SUBJECT = "Your login link for Superlists"
 SUCCESS_MESSAGE = "Check your email, we've sent you a link you can use to log in."
 
 
