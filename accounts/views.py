acca08810d702c5abf42826ef819a9ca35739f94
Philip Vegdahl <pvegdahl@gmail.com>
2021-07-01 10:09:36 -0400

    Remove unused import

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
 
 from accounts.models import Token
