2e6c7b00f6e627267dd1a1f900354c861e86c006
Philip Vegdahl <pvegdahl@gmail.com>
2021-07-01 10:08:24 -0400

    Login URL generated and sent

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from django.test import TestCase
 
+from accounts.models import Token
 from accounts.views import EMAIL_SUBJECT, SUCCESS_MESSAGE
 
 EMAIL = "someone@somewhere.com"
@@ -17,7 +18,6 @@ class SendLoginEmailViewTest(TestCase):
     def test_sends_email_to_address_from_post(self, mock_send_mail):
         self.client.post("/accounts/send_login_email", data={"email": EMAIL})
 
-        self.assertTrue(mock_send_mail.called)
         mock_send_mail.assert_called_once_with(
             subject=EMAIL_SUBJECT,
             message=mock.ANY,
@@ -31,6 +31,18 @@ class SendLoginEmailViewTest(TestCase):
         message = list(response.context["messages"])[0]
         self.assertEqual(message.message, SUCCESS_MESSAGE)
         self.assertEqual(message.tags, "success")
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post("/accounts/send_login_email", data={"email": EMAIL})
+        self.assertEqual(Token.objects.first().email, EMAIL)
+
+    @patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post("/accounts/send_login_email", data={"email": EMAIL})
+
+        # noinspection HttpUrlsUsage
+        expected_url = f"http://testserver/accounts/login?token={Token.objects.first().uid}"
+        self.assertIn(expected_url, mock_send_mail.call_args.kwargs["message"])
 
 
 class LoginViewTest(TestCase):
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
+from accounts.models import Token
 
 EMAIL_SUBJECT = "Your login link for Superlists"
 SUCCESS_MESSAGE = "Check your email, we've sent you a link you can use to log in."
@@ -9,9 +11,11 @@ SUCCESS_MESSAGE = "Check your email, we've sent you a link you can use to log in
 
 def send_login_email(request):
     email = request.POST["email"]
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(reverse("login") + "?token=" + str(token.uid))
     send_mail(
         subject=EMAIL_SUBJECT,
-        message="Use this link to log in",
+        message=f"Use this link to log in\n\n{url}",
         from_email="noreply@superlists",
         recipient_list=[email],
     )
