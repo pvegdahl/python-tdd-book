77c9681e2aabc51542deae18a3e12a9d83bdc238
Philip Vegdahl <pvegdahl@gmail.com>
2021-07-05 14:42:03 -0400

    Add email to settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
